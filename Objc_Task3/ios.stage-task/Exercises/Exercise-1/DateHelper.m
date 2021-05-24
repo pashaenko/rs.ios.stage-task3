1ca271d8eebf8ad95401ea1192341cd8efde8bda
pashaenko <pashaenko@icloud.com>
2021-05-24 22:32:04 +0300

    save change

--- Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
@@ -5,19 +5,36 @@
 #pragma mark - First
 
 -(NSString *)monthNameBy:(NSUInteger)monthNumber {
-    return nil;
-}
+    if (monthNumber < 1 || monthNumber > 12) {
+            return nil;
+        }
+        NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    NSLocale *locale = [[NSLocale alloc]
+                             initWithLocaleIdentifier:@"en_US"];
+        [dateFormatter setLocale:locale];
+        return [[dateFormatter monthSymbols] objectAtIndex:(monthNumber-1)];
+    }
+    
 
 #pragma mark - Second
 
 - (long)dayFromDate:(NSString *)date {
-    return 0;
+    NSDateFormatter* dateFormatter = [[NSDateFormatter alloc]init];
+    [dateFormatter setDateFormat:@"yyyy-MM-dd'T'HH:mm:ss'Z'"];
+    NSDate *parseDate = [dateFormatter dateFromString:date];
+    [dateFormatter setDateFormat:@"dd"];
+    return [dateFormatter stringFromDate:parseDate].longLongValue;
 }
 
 #pragma mark - Third
 
 - (NSString *)getDayName:(NSDate*) date {
-    return nil;
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    NSLocale *locale = [[NSLocale alloc]
+                         initWithLocaleIdentifier:@"ru_RU"];
+    [dateFormatter setLocale:locale];
+    [dateFormatter setDateFormat:@"E"];
+    return [dateFormatter stringFromDate:date];
 }
 
 #pragma mark - Fourth
